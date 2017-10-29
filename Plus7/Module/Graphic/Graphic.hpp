2d4c78cd4b8eff1704ff00f74b0800b5f3f6bf4a
txfx <fx@txfx.org>
2017-10-29 23:05:24 +0100

    Hide SdlGlGraphic behind a generic name

---
+++ Plus7/Module/Graphic/Graphic.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "SdlGlGraphic.hpp"
+
+namespace p7 {
+namespace module {
+using Graphic = SdlGlGraphic;
+}
+} // namespace p7
--- Samples/HelloWorld.cpp
+++ Samples/HelloWorld.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 
 #include <App.hpp>
-#include <Module/Graphic/SdlGlGraphic.hpp>
+#include <Module/Graphic/Graphic.hpp>
 
 int main()
 {
     using p7::App;
-    using p7::module::SdlGlGraphic;
+    using p7::module::Graphic;
 
     App helloWorldApp;
-    helloWorldApp.LoadModule<SdlGlGraphic>("Hello World", 640, 480);
+    helloWorldApp.LoadModule<Graphic>("Hello World", 640, 480);
 
     helloWorldApp.Run();
     std::cout << "Hello World" << std::endl;
