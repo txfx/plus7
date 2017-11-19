2fa9e1495cfddce9fdc8482d8a3e0926f8ef1806
txfx <fx.roure@gmail.com>
2017-11-19 16:32:38 +0100

    Rename RendererInterface -> DummyCommandBuffer

--- Plus7/Modules/Gfx/DummyCommandBuffer.hpp
+++ Plus7/Modules/Gfx/DummyCommandBuffer.hpp
@@ -12,24 +12,21 @@
 namespace p7 {
 namespace gfx {
 
-struct RendererInterface
+struct DummyCommandBuffer
 {
 public:
-    // Device
-    virtual Device& GetDevice() = 0;
-
     // clear
-    virtual void Clear(float _r, float _g, float _b, float _a) = 0;
+    virtual void Clear(float /* _r */, float /* _g */, float /* _b */, float /* _a */) {}
     inline void  Clear(float _r, float _g, float _b) { Clear(_r, _g, _b, 1.0f); }
 
     // viewport & scissors
-    virtual void SetViewport(float _x, float _y, float _w, float _h) = 0;
-    virtual void SetScissor(float _x, float _y, float _w, float _h)  = 0;
+    virtual void SetViewport(float /* _x */, float /* _y */, float /* _w */, float /* _h */) {}
+    virtual void SetScissor(float /* _x */, float /* _y */, float /* _w */, float /* _h */) {}
 
     // Buffers
-    virtual void BindIndexBuffer(const BufferPtr& _buffer, uint32_t _offset, uint32_t _size)                      = 0;
-    virtual void BindVertexBuffer(const BufferPtr& _buffer, uint32_t _offset, uint32_t _size, uint8_t _binding)   = 0;
-    virtual void BindConstantBuffer(const BufferPtr& _buffe, uint32_t _offset, uint32_t _sizer, uint8_t _binding) = 0;
+    virtual void BindIndexBuffer(const BufferPtr& /* _buffer */, uint32_t /* _offset */, uint32_t /* _size */) {}
+    virtual void BindVertexBuffer(const BufferPtr& /* _buffer */, uint32_t /* _offset */, uint32_t /* _size */, uint8_t /* _binding */) {}
+    virtual void BindConstantBuffer(const BufferPtr& /* _buffer */, uint32_t /* _offset */, uint32_t /* _sizer */, uint8_t /* _binding */) {}
 
     inline void BindIndexBuffer(const BufferPtr& _buffer) { BindIndexBuffer(_buffer, 0, _buffer->GetSize()); }
     inline void BindIndexBuffer(const BufferSpan& _span) { BindIndexBuffer(_span.GetBuffer(), _span.GetOffset(), _span.GetSize()); }
@@ -41,18 +38,18 @@ public:
     inline void BindConstantBuffer(const BufferSpan& _span, uint8_t _binding) { BindConstantBuffer(_span.GetBuffer(), _span.GetOffset(), _span.GetSize(), _binding); }
 
     // Render states
-    virtual void BindBlendState(const BlendState& _state)           = 0;
-    virtual void BindDepthState(const DepthState& _state)           = 0;
-    virtual void BindRasterizerState(const RasterizerState& _state) = 0;
+    virtual void BindBlendState(const BlendState& /* _state */) {}
+    virtual void BindDepthState(const DepthState& /* _state */) {}
+    virtual void BindRasterizerState(const RasterizerState& /* _state */) {}
 
-    virtual void BindShaderState(const ShaderState& _state) = 0;
+    virtual void BindShaderState(const ShaderState& /* _state */) {}
 
     // Texture
-    virtual void BindTexture(const TexturePtr& _texture, uint8_t _binding) = 0;
+    virtual void BindTexture(const TexturePtr& /* _texture */, uint8_t /* _binding */) {}
 
     // Draw
-    virtual void Draw(uint32_t _vertexCount, uint32_t _instanceCount, uint32_t _firstVertex, uint32_t _firstInstance)                             = 0;
-    virtual void DrawIndexed(uint32_t _indexCount, uint32_t _instanceCount, uint32_t _firstIndex, int32_t _vertexOffset, uint32_t _firstInstance) = 0;
+    virtual void Draw(uint32_t /* _vertexCount */, uint32_t /* _instanceCount */, uint32_t /* _firstVertex */, uint32_t /* _firstInstance */) {}
+    virtual void DrawIndexed(uint32_t /* _indexCount */, uint32_t /* _instanceCount */, uint32_t /* _firstIndex */, int32_t /* _vertexOffset */, uint32_t /* _firstInstance */) {}
 
     inline void Draw(uint32_t _vertexCount) { Draw(_vertexCount, 1, 0, 0); }
     inline void Draw(uint32_t _vertexCount, uint32_t _instanceCount) { Draw(_vertexCount, _instanceCount, 0, 0); }
--- Plus7/Modules/Gfx/SdlGl/SdlGlRenderer.hpp
+++ Plus7/Modules/Gfx/SdlGl/SdlGlRenderer.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <Module.hpp>
-#include <Modules/Gfx/RendererInterface.hpp>
+#include <Modules/Gfx/DummyCommandBuffer.hpp>
 
 struct SDL_Window;
 using SDL_GLContext = void*;
@@ -9,7 +9,7 @@ using SDL_GLContext = void*;
 namespace p7 {
 namespace gfx {
 
-struct SdlGlRenderer final : public Module, public RendererInterface
+struct SdlGlRenderer final : public Module, public DummyCommandBuffer
 {
 public:
     explicit SdlGlRenderer(App& _app);
@@ -20,19 +20,17 @@ public:
     void Update() override;
 
     // Renderer
-    Device& GetDevice() override { return device; }
-
     void Clear(float _r, float _g, float _b, float _a) override;
 
     void SetViewport(float _x, float _y, float _w, float _h) override;
     void SetScissor(float _x, float _y, float _w, float _h) override;
 
     void BindIndexBuffer(const BufferPtr& _buffer, uint32_t _offset, uint32_t _size) override;
-    using RendererInterface::BindIndexBuffer;
+    using DummyCommandBuffer::BindIndexBuffer;
     void BindVertexBuffer(const BufferPtr& _buffer, uint32_t _offset, uint32_t _size, uint8_t _binding) override;
-    using RendererInterface::BindVertexBuffer;
+    using DummyCommandBuffer::BindVertexBuffer;
     void BindConstantBuffer(const BufferPtr& _buffer, uint32_t _offset, uint32_t _size, uint8_t _binding) override;
-    using RendererInterface::BindConstantBuffer;
+    using DummyCommandBuffer::BindConstantBuffer;
 
     void BindBlendState(const BlendState& _state) override;
     void BindDepthState(const DepthState& _state) override;
@@ -43,9 +41,9 @@ public:
     void BindTexture(const TexturePtr& _texture, uint8_t _binding) override;
 
     void Draw(uint32_t _vertexCount, uint32_t _instanceCount, uint32_t _firstVertex, uint32_t _firstInstance) override;
-    using RendererInterface::Draw;
+    using DummyCommandBuffer::Draw;
     void DrawIndexed(uint32_t _indexCount, uint32_t _instanceCount, uint32_t _firstIndex, int32_t _vertexOffset, uint32_t _firstInstance) override;
-    using RendererInterface::DrawIndexed;
+    using DummyCommandBuffer::DrawIndexed;
 
 private:
     SdlGlRenderer(const char* _name, int _w, int _h, bool _visible, App& _app);
