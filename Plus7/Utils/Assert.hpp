d43dd9956b7d469fbb861f1a6464813b8e03606b
txfx <fx.roure@gmail.com>
2017-11-26 22:19:14 +0100

    Assert macro should have cond inside parentesis

--- Plus7/Utils/Assert.hpp
+++ Plus7/Utils/Assert.hpp
@@ -2,6 +2,8 @@
 
 #include <cassert>
 
-#define P7_ASSERT(cond, ...) \
-    if (!cond)               \
-        __assert(#cond, __FILE__, __LINE__);
+#define P7_ASSERT(cond, ...)                 \
+    if (!(cond))                             \
+    {                                        \
+        __assert(#cond, __FILE__, __LINE__); \
+    }
