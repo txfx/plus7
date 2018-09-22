b3522a12bda7c4ad7a1671064dbd7f0f514b0473
txfx <fx.roure@gmail.com>
2018-09-22 17:42:53 +0200

    disable asset in release

--- Plus7/Utils/Assert.hpp
+++ Plus7/Utils/Assert.hpp
@@ -2,8 +2,12 @@
 
 #include <cassert>
 
+#ifndef NDEBUG
 #define P7_ASSERT(cond, ...)                 \
     if (!(cond))                             \
     {                                        \
         __assert(#cond, __FILE__, __LINE__); \
     }
+#else // NDEBUG
+#define P7_ASSERT(cond, ...) (void)(cond)
+#endif // NDEBUG
