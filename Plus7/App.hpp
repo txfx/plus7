ccd91cfd5d25527e6cc598fa4ba35420d6e26d05
txfx <fx.roure@gmail.com>
2018-09-22 16:58:51 +0200

    Put definition of templated methods outside of the class declaration

--- Plus7/App.hpp
+++ Plus7/App.hpp
@@ -18,34 +18,46 @@ public:
     void Run();
 
     template <typename T, typename... TArgs>
-    T& LoadModule(TArgs... args)
-    {
-        modules.emplace_back(std::make_unique<T>(args..., *this));
-        return dynamic_cast<T&>(*modules.back());
-    }
-
+    T& LoadModule(TArgs... args);
     template <typename T>
-    T& GetDependency()
-    {
-        auto it = std::find_if(modules.begin(), modules.end(), [](const auto& mod) {
-            return dynamic_cast<T*>(mod.get()) != nullptr;
-        });
+    T& GetDependency();
 
-        if (it != modules.end())
-        {
-            return *dynamic_cast<T*>(it->get());
-        }
-        else
-        {
-            return LoadModule<T>();
-        }
-    }
-
+    auto GetDummyTask() const { return dummyTask; }
     void RequestExit();
 
 private:
     bool exit_requested = false;
 
+    tasks::ID<void> dummyTask;
+
     std::vector<std::unique_ptr<Module>> modules;
-}; // namespace p7
+};
+
+//==============================================================================================
+// App Implementation
+//==============================================================================================
+template <typename T, typename... TArgs>
+T& App::LoadModule(TArgs... args)
+{
+    modules.emplace_back(std::make_unique<T>(args..., *this));
+    return dynamic_cast<T&>(*modules.back());
+}
+
+template <typename T>
+T& App::GetDependency()
+{
+    auto it = std::find_if(modules.begin(), modules.end(), [](const auto& mod) {
+        return dynamic_cast<T*>(mod.get()) != nullptr;
+    });
+
+    if (it != modules.end())
+    {
+        return *dynamic_cast<T*>(it->get());
+    }
+    else
+    {
+        return LoadModule<T>();
+    }
+}
+
 } // namespace p7
