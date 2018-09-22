6c9f1d90c89dc92e3169228db7b3ea948116585d
txfx <fx.roure@gmail.com>
2018-09-22 20:04:26 +0200

    support one frame button down

--- Plus7/Modules/Inputs/Sdl/SdlMouse.cpp
+++ Plus7/Modules/Inputs/Sdl/SdlMouse.cpp
@@ -18,22 +18,37 @@ SdlMouse::SdlMouse(App& _app)
 MouseState SdlMouse::PollEvents()
 {
     MouseState state;
-    SDL_Event  event;
 
-    if (SDL_PeepEvents(&event, 1, SDL_eventaction::SDL_GETEVENT, SDL_MOUSEWHEEL, SDL_MOUSEWHEEL) == 1)
+    SDL_Event event;
+    while (SDL_PeepEvents(&event, 1,
+               SDL_eventaction::SDL_GETEVENT,
+               SDL_EventType::SDL_MOUSEMOTION,
+               SDL_EventType::SDL_MOUSEWHEEL)
+        == 1)
     {
-        state.wheel.horizontal = event.wheel.x;
-        state.wheel.vertical   = event.wheel.y;
+        switch (event.type)
+        {
+        case SDL_EventType::SDL_MOUSEWHEEL:
+            state.wheel.horizontal = event.wheel.x;
+            state.wheel.vertical   = event.wheel.y;
+            break;
+
+        case SDL_EventType::SDL_MOUSEBUTTONDOWN:
+            state.buttons[0] |= event.button.button == SDL_BUTTON_LEFT;
+            state.buttons[1] |= event.button.button == SDL_BUTTON_RIGHT;
+            state.buttons[2] |= event.button.button == SDL_BUTTON_MIDDLE;
+            break;
+        }
     }
 
     int    mx, my;
     Uint32 mouseMask = SDL_GetMouseState(&mx, &my);
 
-    state.pos.x      = (float)mx;
-    state.pos.y      = (float)my;
-    state.buttons[0] = (mouseMask & SDL_BUTTON(SDL_BUTTON_LEFT)) != 0;
-    state.buttons[1] = (mouseMask & SDL_BUTTON(SDL_BUTTON_RIGHT)) != 0;
-    state.buttons[2] = (mouseMask & SDL_BUTTON(SDL_BUTTON_MIDDLE)) != 0;
+    state.pos.x = (float)mx;
+    state.pos.y = (float)my;
+    state.buttons[0] |= (mouseMask & SDL_BUTTON(SDL_BUTTON_LEFT)) != 0;
+    state.buttons[1] |= (mouseMask & SDL_BUTTON(SDL_BUTTON_RIGHT)) != 0;
+    state.buttons[2] |= (mouseMask & SDL_BUTTON(SDL_BUTTON_MIDDLE)) != 0;
 
     return state;
 }
