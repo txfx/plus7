c7b9f0faba698c9bbba04360fe00e0d1fb8bcace
txfx <fx.roure@gmail.com>
2020-04-11 17:24:27 +0200

    Upgrade ImGui to version 1.75

--- Samples/HelloWorld.cpp
+++ Samples/HelloWorld.cpp
@@ -30,7 +30,7 @@ int main()
             ::ImGui::ColorEdit3("clear color", (float*)&clear_color);
             ::ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ::ImGui::GetIO().Framerate, ::ImGui::GetIO().Framerate);
 
-            ::ImGui::ShowTestWindow();
+            ::ImGui::ShowDemoWindow();
 
             using p7::gfx::RasterizerState;
             static RasterizerState defaultState({});
