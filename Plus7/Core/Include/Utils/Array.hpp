15f041051c7319dcfb47cad55c6f2e68638ee6b4
txfx <fx.roure@gmail.com>
2022-09-19 19:08:50 +0200

    std::array concat without using integer sequence. It's a lot more readable

--- Plus7/Core/Include/Utils/Array.hpp
+++ Plus7/Core/Include/Utils/Array.hpp
@@ -6,20 +6,21 @@
 
 namespace p7 {
 
-template <typename T, std::size_t N1, std::size_t N2, std::size_t... IDs>
-constexpr auto concat(
-    const std::array<T, N1>& array1,
-    const std::array<T, N2>& array2,
-    std::integer_sequence<std::size_t, IDs...>)
-{
-    constexpr auto get = [](const auto& a1, const auto& a2, auto pos) { return pos < N1 ? a1[pos] : a2[pos - N1]; };
-    return std::array<T, N1 + N2> { get(array1, array2, IDs)... };
-}
-
 template <typename T, std::size_t N1, std::size_t N2>
 constexpr std::array<T, N1 + N2> concat(const std::array<T, N1>& array1, const std::array<T, N2>& array2)
 {
-    return concat(array1, array2, std::make_integer_sequence<std::size_t, N1 + N2> {});
+    std::array<T, N1 + N2> ret { {} };
+
+    int i = 0;
+    for (T e : array1)
+    {
+        ret[i++] = e;
+    }
+    for (T e : array2)
+    {
+        ret[i++] = e;
+    }
+    return ret;
 }
 
 } // namespace p7
